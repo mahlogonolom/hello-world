ddc142ee116f9cfdf9057e476be250220218feb2
mahlogonolom <112306530+mahlogonolom@users.noreply.github.com>
2022-08-28 09:23:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DOEFA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World this is just a basic applcation!");
+        }
+    }
+}
